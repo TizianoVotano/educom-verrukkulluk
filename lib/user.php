74d8d49a0a36714f67b0bc8a6db155c75eefd478
TizianoVotano <tiziano_votano@hotmail.com>
2023-06-28 15:26:35 +0200

    edited user.php

--- lib/user.php
+++ lib/user.php
@@ -8,7 +8,7 @@ class User {
     }
 
     public function selectUser($user_id) {
-        $sql = "select user_name, email from user where id = $user_id";
+        $sql = "select * from user where id = $user_id";
 
         $result = mysqli_query($this->connection, $sql);
         $user = mysqli_fetch_array($result, MYSQLI_ASSOC);
