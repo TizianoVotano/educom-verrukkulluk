b5c34d1a7eeae878b7c92b2378435aa0d0852589
TizianoVotano <tiziano_votano@hotmail.com>
2023-07-04 12:38:16 +0200

    Vervaardigen Recipe class (#10)

--- lib/recipe.php
+++ lib/recipe.php
@@ -7,14 +7,14 @@ require_once("kitchen_type.php");
 class Recipe {
     private $connection;
     private $user;
-    private $ingredient;
+    private $ingredients;
     private $recipe_info;
     private $kitchen_type;
 
     public function __construct($connection) {
         $this->connection = $connection;
         $this->user = new User($connection);
-        $this->ingredient = new Ingredient($connection);
+        $this->ingredients = new Ingredient($connection);
         $this->recipe_info = new RecipeInfo($connection);
         $this->kitchen_type = new KitchenType($connection);
     }
@@ -24,19 +24,24 @@ class Recipe {
         $result = mysqli_query($this->connection, $sql);
         $recipe = mysqli_fetch_array($result, MYSQLI_ASSOC);
 
+        $user = $this->selectUser($recipe["user_id"]);
+        $kitchen = $this->selectKitchen($recipe["keuken_id"]); 
+        $type = $this->selectType($recipe["type_id"]);
+        $ingredients = $this->selectIngredients($recipe["id"]);
+        $recipe_info = $this->selectRecipeInfo($recipe["id"]);
 
-        $this->selectKitchen($recipe["keuken_id"]); 
-        $this->selectType($recipe["type_id"]);
-        $this->selectIngredients($recipe["id"]);
-        $this->selectRecipeInfo($recipe["id"]);
+        $price = $this->calcPrice($ingredients);
+        $calories = $this->calcCalories($ingredients);
+
+        $returnRecipe = ["user"=>$user, "price"=>$price, "calories"=>$calories, "recipe"=>$recipe, 
+                        "recipe_info"=>$recipe_info, "kitchen"=>$kitchen, "type"=>$type, "ingredients"=>$ingredients];
 
         echo "<pre>";
-        print_r($recipe);
+        print_r($returnRecipe);
         echo "</pre>";
         return($recipe);
     }
 
-    // hoorde dit niet tot GerechtInfo?
     private function selectUser($user_id) {
         return $this->user->selectUser($user_id);
     }
@@ -46,15 +51,37 @@ class Recipe {
     }
 
     private function selectIngredients($recipe_id) {
-        return $this->ingredient->selectIngredients($recipe_id);
+        return $this->ingredients->selectIngredients($recipe_id);
     }
 
-    private function calcCalories() {
-        
+    private function calcCalories($ingredients) {
+        $calories = 0;
+        foreach ($ingredients as $ingredient) {
+            $quantity = $ingredient["aantal"];
+            $unit = $ingredient["artikel"]["eenheid"];
+            $unitCalorie = $ingredient["artikel"]["calories"];
+
+            if ($quantity < 0)
+                $quantity = 0;
+            $calories += ceil($quantity / $unit) * $unitCalorie;
+        }
+
+        return $calories;
     }
 
-    private function calcPrice() {
-        
+    private function calcPrice($ingredients) {        
+        $price = 0;
+        foreach ($ingredients as $ingredient) {
+            $quantity = $ingredient["aantal"];
+            $unit = $ingredient["artikel"]["eenheid"];
+            $unitPrice = $ingredient["artikel"]["prijs"];
+
+            if ($quantity < 0)
+                $quantity = 0;
+            $price += ceil($quantity / $unit) * $unitPrice;
+        }
+
+        return $price;
     }
 
     private function selectRating() {
@@ -78,7 +105,13 @@ class Recipe {
         return $this->kitchen_type->selectKitchenType($id);
     }
 
-    private function determineFavourite() {
-
+    public function determineFavourite($recipe_id, $user_id) {
+        $favourites = $this->selectRecipeInfo($recipe_id)["favoriet"];
+        foreach ($favourites as $favourite) {
+            if($favourite["gerecht_id"] == $recipe_id && $favourite["user_id"] == $user_id){
+                return true;
+            }
+        }
+        return false;
     }
 }
