6c8096fd0e15eff84be48f87d2273116ca154428
TizianoVotano <tiziano_votano@hotmail.com>
2023-07-18 11:33:13 +0200

    added require_once for article

--- index.php
+++ index.php
@@ -1,18 +1,13 @@
 <?php
 
 require_once("lib/database.php");
-require_once("lib/article.php");
-require_once("lib/user.php");
-require_once("lib/kitchen_type.php");
-require_once("lib/ingredient.php");
-require_once("lib/recipe_info.php");
 require_once("lib/recipe.php");
 require_once("lib/boodschappen.php");
 
 /// INIT
 $db = new Database();
 $recipe = new Recipe($db->getConnection());
-$commisions = new Boodschappen($db->getConnection());
+// $commisions = new Boodschappen($db->getConnection());
 
 
 /// VERWERK 
@@ -21,3 +16,70 @@ $commisions = new Boodschappen($db->getConnection());
 // $dataComissions = $commisions->boodschappenToevoegen(2, 2);
 
 /// RETURN
+
+
+
+
+
+
+//// Allereerst zorgen dat de "Autoloader" uit vendor opgenomen wordt:
+require_once("./vendor/autoload.php");
+
+/// Twig koppelen:
+$loader = new \Twig\Loader\FilesystemLoader("./templates");
+/// VOOR PRODUCTIE:
+/// $twig = new \Twig\Environment($loader), ["cache" => "./cache/cc"]);
+
+/// VOOR DEVELOPMENT:
+$twig = new \Twig\Environment($loader, ["debug" => true ]);
+$twig->addExtension(new \Twig\Extension\DebugExtension());
+
+/******************************/
+
+/// Next step, iets met je data doen. Ophalen of zo
+require_once("fase-2/lib/gerecht.php");
+require_once("./lib/recipe.php");
+require_once("./lib/recipe.php");
+$recipe = new Recipe($db->getConnection());
+$data = $recipe->selectRecipe();
+//var_dump($data);
+
+/*
+URL:
+http://localhost/index.php?gerecht_id=4&action=detail
+*/
+
+$gerecht_id = isset($_GET["gerecht_id"]) ? $_GET["gerecht_id"] : "";
+$action = isset($_GET["action"]) ? $_GET["action"] : "homepage";
+
+$template = "homepage.html.twig";
+$title = "homepage";
+
+switch($action) {
+
+        case "homepage": {
+            $data = $recipe->selectRecipe();
+            $template = 'homepage.html.twig';
+            $title = "homepage";
+            break;
+        }
+
+        case "detail": {
+            $data = $recipe->selectRecipe($gerecht_id);
+            $template = 'detail.html.twig';
+            $title = "detail pagina";
+            break;
+        }
+
+        /// etc
+
+}
+
+
+/// Onderstaande code schrijf je idealiter in een layout klasse of iets dergelijks
+/// Juiste template laden, in dit geval "homepage"
+$templ = $twig->load($template);
+
+
+/// En tonen die handel!
+echo $templ->render(["title" => $title, "data" => $data]);
--- lib/ingredient.php
+++ lib/ingredient.php
@@ -1,4 +1,5 @@
 <?php
+require_once("article.php");
 class Ingredient {
     private $connection;
     private $article;
