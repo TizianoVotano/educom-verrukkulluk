3159264c18d61fe0469a316781581820c8d7c671
TizianoVotano <tiziano_votano@hotmail.com>
2023-07-19 09:31:18 +0200

    homepagina uitgebreid (#13)

--- index.php
+++ index.php
@@ -1,27 +1,4 @@
 <?php
-
-require_once("lib/database.php");
-require_once("lib/recipe.php");
-require_once("lib/boodschappen.php");
-
-/// INIT
-$db = new Database();
-$recipe = new Recipe($db->getConnection());
-// $commisions = new Boodschappen($db->getConnection());
-
-
-/// VERWERK 
- $dataAllRecipes = $recipe->selectRecipe();
- $recipe->findRecipes($dataAllRecipes, "bolog look pasta");
-// $dataComissions = $commisions->boodschappenToevoegen(2, 2);
-
-/// RETURN
-
-
-
-
-
-
 //// Allereerst zorgen dat de "Autoloader" uit vendor opgenomen wordt:
 require_once("./vendor/autoload.php");
 
@@ -37,18 +14,22 @@ $twig->addExtension(new \Twig\Extension\DebugExtension());
 /******************************/
 
 /// Next step, iets met je data doen. Ophalen of zo
-require_once("fase-2/lib/gerecht.php");
+require_once("./lib/database.php");
 require_once("./lib/recipe.php");
-require_once("./lib/recipe.php");
+$db = new Database(); 
 $recipe = new Recipe($db->getConnection());
 $data = $recipe->selectRecipe();
-//var_dump($data);
 
-/*
-URL:
-http://localhost/index.php?gerecht_id=4&action=detail
-*/
+// SEARCHBAR TEST
+// $data = $recipe->selectRecipe();
+// $dataAllRecipes = $recipe->selectRecipe();
+// $recipe->findRecipes($dataAllRecipes, "bolog look pasta");
+// FINAL TEST
+// require_once("lib/boodschappen.php");
+// $commisions = new Boodschappen($db->getConnection());
+// $dataComissions = $commisions->boodschappenToevoegen(2, 2);
 
+// URL: http://localhost/index.php?gerecht_id=4&action=detail
 $gerecht_id = isset($_GET["gerecht_id"]) ? $_GET["gerecht_id"] : "";
 $action = isset($_GET["action"]) ? $_GET["action"] : "homepage";
 
@@ -56,30 +37,27 @@ $template = "homepage.html.twig";
 $title = "homepage";
 
 switch($action) {
+    case "homepage": {
+        $data = $recipe->selectRecipe();
+        $template = 'homepage.html.twig';
+        $title = "homepage";
+        break;
+    }
 
-        case "homepage": {
-            $data = $recipe->selectRecipe();
-            $template = 'homepage.html.twig';
-            $title = "homepage";
-            break;
-        }
-
-        case "detail": {
-            $data = $recipe->selectRecipe($gerecht_id);
-            $template = 'detail.html.twig';
-            $title = "detail pagina";
-            break;
-        }
+    case "detail": {
+        $data = $recipe->selectRecipe($gerecht_id);
+        $template = 'detail.html.twig';
+        $title = "detailpage";
+        break;
+    }
 
         /// etc
-
 }
 
 
 /// Onderstaande code schrijf je idealiter in een layout klasse of iets dergelijks
 /// Juiste template laden, in dit geval "homepage"
 $templ = $twig->load($template);
-
 
 /// En tonen die handel!
 echo $templ->render(["title" => $title, "data" => $data]);
